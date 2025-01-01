9855de1e731fcc9a050dd286d6d692eb9253d7db
Itskomalgund <komalsgund123@gmail.com>
2025-01-01 19:15:50 +0530

    understanding branching mechanism

---
+++ komal.py
@@ -0,0 +1,4 @@
+# git is very important
+# git add <python file name> - this command is used to  add in git
+# git commit -m "commit msg" - this command is used to commit a code
+# git push origin main - this command is used to push the code on github.com
